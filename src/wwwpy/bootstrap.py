606f65e153f1125a79bfe492212d5caccc975969
Simone <simone.giacomelli@gmail.com>
2023-03-24 19:07:54 +0000

    bootstrap python

---
+++ src/wwwpy/bootstrap.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Bootstrap:
+    def __init__(self) -> None:
+        self._python: str = ''
+
+    def add_python(self, code: str) -> None:
+        self._python = code
+
+    def get_javascript(self) -> str:
+        return _js_content.replace('# python replace marker', self._python)
+
+
+_js_content = """
+if (typeof loadPyodide === 'undefined') {
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/pyodide/v0.22.1/full/pyodide.js';
+    script.onload = async () => {
+        let pyodide = await loadPyodide();
+        window.pyodide = pyodide;
+        console.log('loading pyodide.runPythonAsync(...). See in the following lines for the code');
+        console.log('-----------------------  START PYTHON CODE  -------------------------------');
+        console.log(`# python replace marker`);
+        console.log('-----------------------  END PYTHON CODE    -------------------------------');
+        pyodide.runPythonAsync(`# python replace marker`);
+    };
+    document.body.append(script)
+}
+"""
--- tests/layer/test_layer_1.py
+++ tests/layer/test_layer_1.py
@@ -3,6 +3,7 @@ from time import sleep
 from tests import for_all_webservers
 from playwright.sync_api import Page, expect
 
+from wwwpy.bootstrap import Bootstrap
 from wwwpy.http_response import HttpResponse
 from wwwpy.http_route import HttpRoute
 from wwwpy.webserver import Webserver
@@ -15,3 +16,14 @@ def test_javascript_execution(page: Page, webserver: Webserver):
     webserver.start_listen()
     page.goto(webserver.localhost_url())
     expect(page.locator('id=id1')).to_have_id('id1')
+
+
+@for_all_webservers()
+def test_python_execution(page: Page, webserver: Webserver):
+    bootstrap = Bootstrap()
+    bootstrap.add_python('from js import document\ndocument.getElementById("tag1").value = "foo1"')
+    html = f'<input id="tag1" value="bar"><script>{bootstrap.get_javascript()}</script>'
+    webserver.set_http_route(HttpRoute('/', lambda request: HttpResponse.text_html(html)))
+    webserver.start_listen()
+    page.goto(webserver.localhost_url())
+    expect(page.locator('id=tag1')).to_have_value('foo1')
