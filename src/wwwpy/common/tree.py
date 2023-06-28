1bd3125479f7076d8cc3e1d7f0189a1a71fb7ac2
Simone <simone.giacomelli@gmail.com>
2023-06-28 21:15:03 +0100

    utility to print a fs tree in ascii

---
+++ src/wwwpy/common/tree.py
@@ -0,0 +1,36 @@
+# tree.py
+# credits to: https://stackoverflow.com/a/59109706/316766
+
+from pathlib import Path
+
+# prefix components:
+space = '    '
+branch = '│   '
+# pointers:
+tee = '├── '
+last = '└── '
+
+
+def tree(dir_path: Path, prefix: str = ''):
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():  # extend the prefix and recurse:
+            extension = branch if pointer == tee else space
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix + extension)
+
+
+def print_tree(path):
+    for line in tree(Path(path)):
+        print(line)
+
+
+if __name__ == '__main__':
+    print_tree(Path(__file__).parent.parent)
