9b975c47437615dd2ea475f865e0d9c4bdbed5f2
Simone <simone.giacomelli@gmail.com>
2023-07-22 16:59:08 +0100

    set_timeout order is not guaranteed - fix

--- tests/remote_conftest.py
+++ tests/remote_conftest.py
@@ -1,14 +1,17 @@
 from js import window
 from pyodide.ffi import create_once_callable
 
+_events = []
+
 
 def pytest_xvirt_send_event(event_json):
     print('siamo arrivati qui')
 
     async def callback():
         path = '#xvirt_notify_path_marker#'
-        await async_fetch_str(path, method='POST', data=event_json)
+        await async_fetch_str(path, method='POST', data=_events.pop(0))
 
+    _events.append(event_json)
     set_timeout(callback)
 
 
