15a9e50cec1df35dbf3caa78e83925a46997a2d6
Simone <simone.giacomelli@gmail.com>
2023-10-06 18:39:48 +0100

    update deps

---
+++ pip-intall-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install -r requirements-dev.txt
+pip install -r requirements-test.txt
+pip install -r requirements-pypi.txt
+pip install -e .
+
+# maybe these are better
+# pip install  .[dev]
+# pip install  .[test]
