09d4c519137dff70e82e405824baf9d1d7fccc2a
Simone <simone.giacomelli@gmail.com>
2023-10-06 19:10:04 +0100

    restored fastapi wrapper

--- src/wwwpy/webservers/fastapi.py
+++ src/wwwpy/webservers/fastapi.py
@@ -2,12 +2,11 @@ from threading import Thread
 from typing import Optional
 
 import uvicorn
+from fastapi import FastAPI
 from fastapi.responses import Response
 
-import wwwpy
-from fastapi import FastAPI
-from wwwpy import Webserver
-from wwwpy.routes import Route
+from wwwpy.http import HttpRoute, HttpResponse, HttpRequest
+from ..webserver import Webserver, wait_forever
 
 
 class WsFastapi(Webserver):
@@ -16,13 +15,18 @@ class WsFastapi(Webserver):
         self.app = FastAPI()
         self.thread: Optional[Thread] = None
 
-    def _setup_route(self, route: Route):
-        def func(*args) -> Response:
-            return self.to_native_response(route.callback())
+    def _setup_route(self, route: HttpRoute):
+        async def func(req) -> Response:
+            if req.method == 'POST':
+                body = await req.body()
+                wwwpy_req = HttpRequest(req.method, body, req.headers)
+            else:
+                wwwpy_req = HttpRequest(req.method, req.body, req.headers)
+            return self.to_native_response(route.callback(wwwpy_req))
 
-        self.app.add_route(route.path, route=func)
+        self.app.add_route(route.path, route=func, methods=['GET','POST'])
 
-    def to_native_response(self, pn_response: wwwpy.HttpResponse):
+    def to_native_response(self, pn_response: HttpResponse):
         return Response(content=pn_response.content, media_type=pn_response.content_type)
 
     def _start_listen(self):
@@ -31,3 +35,10 @@ class WsFastapi(Webserver):
 
         self.thread = Thread(target=run, daemon=True)
         self.thread.start()
+
+
+if __name__ == '__main__':
+    s = WsFastapi()
+    s.set_http_route(HttpRoute('/', lambda req: HttpResponse('ciao', 'text/html')))
+    s.start_listen()
+    wait_forever()
