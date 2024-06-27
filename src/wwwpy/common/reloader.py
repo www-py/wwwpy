027a37e34e21fb87b5cbd36d6aed31fb6ea8681d
Simone <simone.giacomelli@gmail.com>
2024-06-27 21:15:08 +0100

    refactoring

--- src/wwwpy/common/reloader.py
+++ src/wwwpy/common/reloader.py
@@ -2,3 +2,9 @@ def reload(module):
     import importlib
     # importlib.invalidate_caches()
     return importlib.reload(module)
+
+
+def find_package_location(package_name):
+    import importlib.util
+    spec = importlib.util.find_spec(package_name)
+    return spec.origin if spec else None
--- tests/layer/test_layer_5.py
+++ tests/layer/test_layer_5.py
@@ -7,23 +7,24 @@ from wwwpy.server import configure
 from wwwpy.webserver import Webserver
 
 
-@for_all_webservers()
-def test_rpc(page: Page, webserver: Webserver):
-    configure.convention(Path(__file__).parent / 'layer_5_support', webserver)
-    webserver.start_listen()
+class TestServerRpc:
+    @for_all_webservers()
+    def test_rpc(self, page: Page, webserver: Webserver):
+        file_parent = Path(__file__).parent
+        configure.convention(file_parent / 'layer_5_support', webserver)
+        webserver.start_listen()
 
-    page.goto(webserver.localhost_url())
-    expect(page.locator('body')).to_have_text('42')
+        page.goto(webserver.localhost_url())
+        expect(page.locator('body')).to_have_text('42')
 
+    @for_all_webservers()
+    def test_rpc_issue_double_load(self, page: Page, webserver: Webserver):
+        # related to the stubber to being loaded twice
+        configure.convention(Path(__file__).parent / 'layer_5_support', webserver)
+        webserver.start_listen()
 
-@for_all_webservers()
-def test_rpc_issue_double_load(page: Page, webserver: Webserver):
-    # related to the stubber to being loaded twice
-    configure.convention(Path(__file__).parent / 'layer_5_support', webserver)
-    webserver.start_listen()
+        page.goto(webserver.localhost_url())
+        expect(page.locator('body')).to_have_text('42')
 
-    page.goto(webserver.localhost_url())
-    expect(page.locator('body')).to_have_text('42')
-
-    page.goto(webserver.localhost_url())
-    expect(page.locator('body')).to_have_text('42')
+        page.goto(webserver.localhost_url())
+        expect(page.locator('body')).to_have_text('42')
