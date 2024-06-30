3feaa4a2c034a23dd180c78bc240fc67e357ed93
Simone <simone.giacomelli@gmail.com>
2024-07-01 00:35:48 +0100

    comment

--- pip-intall-all.sh
+++ pip-intall-all.sh
@@ -5,6 +5,6 @@ pip install -r requirements-test.txt
 pip install -r requirements-pypi.txt
 pip install -e .
 
-# maybe these are better
+# maybe these are better; but the above fixes the version of the unbounded dependencies
 # pip install  .[dev]
 # pip install  .[test]
