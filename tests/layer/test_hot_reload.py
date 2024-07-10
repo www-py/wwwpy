462263066c3941d7b784324f0a48a53785f08e09
Simone <simone.giacomelli@gmail.com>
2024-07-10 15:38:01 +0100

    disable test rewrite - blind fix

--- tests/layer/test_hot_reload.py
+++ tests/layer/test_hot_reload.py
@@ -1,3 +1,4 @@
+"""PYTEST_DONT_REWRITE"""
 from pathlib import Path
 
 from playwright.sync_api import Page, expect
@@ -9,9 +10,6 @@ from wwwpy.webserver import Webserver
 
 @for_all_webservers()
 def test_hot_reload__modified(page: Page, webserver: Webserver, tmp_path, restore_sys_path):
-    """In this test we will not use pytester even though we are going to modify the code on the disk
-    This could leave the local repo dirty, but it is more convenient than using pytester"""
-
     remote_init = tmp_path / 'remote' / '__init__.py'
     remote_init.parent.mkdir(parents=True)
     remote_init.write_text("from js import document;document.body.innerHTML = 'ready'")
@@ -24,3 +22,32 @@ def test_hot_reload__modified(page: Page, webserver: Webserver, tmp_path, restor
     # change server side file, should reflect on the client side
     remote_init.write_text("from js import document;document.body.innerHTML = 'modified'")
     expect(page.locator('body')).to_have_text('modified')
+
+
+@for_all_webservers()
+def dis_test_hot_reload__created(page: Page, webserver: Webserver, tmp_path, restore_sys_path):
+    remote = tmp_path / 'remote'
+    remote.mkdir()
+
+    (remote / '__init__.py').write_text(_created_python)
+    configure.convention(tmp_path, webserver, dev_mode=True)
+    webserver.start_listen()
+
+    page.goto(webserver.localhost_url())
+    expect(page.locator('body')).to_have_text('ready')
+
+    (remote / 'component1.py').write_text("from js import document;document.body.innerHTML = 'created'")
+    expect(page.locator('body')).to_have_text('created')
+
+
+# language=python
+
+_created_python = """
+try:
+    import component1
+except:
+    pass    
+    
+from js import document
+document.body.innerHTML = 'ready'
+"""
