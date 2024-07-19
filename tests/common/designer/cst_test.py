1423ca7bbad589ffd5f38ada54469aa29ddc93b0
Simone <simone.giacomelli@gmail.com>
2024-07-19 11:33:35 +0100

    designer parser

---
+++ tests/common/designer/cst_test.py
@@ -0,0 +1,27 @@
+from wwwpy.common.designer import cst
+from wwwpy.common.designer.cst import ClassInfo, AttrInfo, SourceInfo
+
+
+def test_info():
+    target = cst.source_info(
+        """
+import wwwpy.remote.component as wpc
+
+class MyElement(wpc.Component):
+    btn1: HTMLButtonElement = wpc.element()
+    """
+    )
+
+    expect = SourceInfo(classes=[ClassInfo('MyElement', [AttrInfo('btn1', 'HTMLButtonElement', 'wpc.element()')])])
+
+    assert target == expect
+#
+# def test_add_attribute():
+#     source_code = """
+# import wwwpy.remote.component as wpc
+#
+# class MyElement(wpc.Component):
+#     pass
+#     """
+#
+#
