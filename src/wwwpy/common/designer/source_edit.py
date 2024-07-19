c8ebe15449cae567bfc5f4e22bc50df6cbce05ac
Simone <simone.giacomelli@gmail.com>
2024-07-19 18:23:41 +0100

    designer parser - edit html

--- src/wwwpy/common/designer/source_edit.py
+++ src/wwwpy/common/designer/source_edit.py
@@ -1,7 +1,7 @@
 import ast
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Callable
 
 
 @dataclass
@@ -80,3 +80,25 @@ def add_attribute(source_code: str, attr_info: Attribute):
     modified_tree = module.visit(transformer)
 
     return modified_tree.code
+
+
+from typing import Callable
+import libcst as cst
+
+def html_edit(source_code: str, html_manipulator: Callable[[str], str]) -> str:
+    tree = cst.parse_module(source_code)
+    transformer = HTMLStringUpdater(html_manipulator)
+    modified_tree = tree.visit(transformer)
+    return modified_tree.code
+
+class HTMLStringUpdater(cst.CSTTransformer):
+    def __init__(self, html_manipulator: Callable[[str], str]):
+        super().__init__()
+        self.html_manipulator = html_manipulator
+
+    def leave_SimpleString(self, original_node: cst.SimpleString, updated_node: cst.SimpleString) -> cst.CSTNode:
+        if original_node.value.startswith('"""') and original_node.value.endswith('"""'):
+            original_html = original_node.value[3:-3]
+            modified_html = self.html_manipulator(original_html)
+            return updated_node.with_changes(value=f'"""{modified_html}"""')
+        return updated_node
--- tests/common/designer/source_edit_test.py
+++ tests/common/designer/source_edit_test.py
@@ -1,4 +1,4 @@
-from wwwpy.common.designer.source_edit import ClassInfo, Attribute, Info, info, add_attribute
+from wwwpy.common.designer.source_edit import ClassInfo, Attribute, Info, info, add_attribute, html_edit
 
 
 def test_info():
@@ -59,3 +59,26 @@ class MyElement(wpc.Component): # comment2
     modified_source = add_attribute(original_source, Attribute('btn2', 'HTMLButtonElement', 'wpc.element()'))
 
     assert modified_source == expected_source
+
+
+def test_html_edit():
+    common_piece = '''
+import wwwpy.remote.component as wpc
+# comment1
+class MyElement(wpc.Component): # comment2
+    btn1: HTMLButtonElement = wpc.element()
+    def connectedCallback(self):        
+        self.element.innerHTML = """
+        <div>my-element5 a</div>
+        <button data-name='btn1'>list async tasks</button>
+        '''
+
+    original_source = common_piece + '"""'
+    exepct_source = common_piece + '''<button data-name='btn2'>list files in folder</button>"""'''
+
+    def manipulate_html(html):
+        return html + "<button data-name='btn2'>list files in folder</button>"
+
+    actual_source = html_edit(original_source, manipulate_html)
+
+    assert actual_source == exepct_source
