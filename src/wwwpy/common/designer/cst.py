5cdbd89e0d9d4e1d8a39971101518ac102dac3ef
Simone <simone.giacomelli@gmail.com>
2024-07-19 11:49:30 +0100

    designer parser - alternative impl with std ast module

--- src/wwwpy/common/designer/cst.py
+++ src/wwwpy/common/designer/cst.py
@@ -1,98 +1,42 @@
+import ast
+
 from dataclasses import dataclass
 from typing import List
 
-import libcst as cst
-import libcst
 
-class AddFieldToClassTransformer(cst.CSTTransformer):
-    def __init__(self, class_name, new_field):
-        super().__init__()
-        self.class_name = class_name
-        self.new_field = new_field
-
-    def leave_ClassDef(self, original_node, updated_node):
-        if original_node.name.value == self.class_name:
-            new_field_node = cst.SimpleStatementLine([
-                cst.AnnAssign(
-                    target=cst.Name(self.new_field['name']),
-                    annotation=cst.Annotation(cst.Name(self.new_field['type'])),
-                    value=cst.Integer(self.new_field['default'])  # Changed from cst.Name to cst.Integer
-                )
-            ])
-            return updated_node.with_changes(
-                body=updated_node.body.with_changes(body=[new_field_node] + list(updated_node.body.body)))
-        return updated_node
-
-
-def main():
-    # Define the new field you want to add
-    new_field = {
-        'name': 'new_field',
-        'type': 'int',
-        'default': '0'
-    }
-
-    # Parse the source code
-    source_code = """
-class MyClass:
-    ...
-    """
-
-    module = cst.parse_module(source_code)
-    transformer = AddFieldToClassTransformer("MyClass", new_field)
-    modified_tree = module.visit(transformer)
-
-    # Print the modified code
-    print(modified_tree.code)
-
-
-if __name__ == "__main__":
-    main()
-
-
-@dataclass()
+@dataclass
 class AttrInfo:
     name: str
     type: str
     default: str
 
-
-@dataclass()
+@dataclass
 class ClassInfo:
     name: str
     attributes: List[AttrInfo]
 
-
-@dataclass()
+@dataclass
 class SourceInfo:
     classes: List[ClassInfo]
 
-import libcst as cst
-
-class ClassInfoExtractor(cst.CSTVisitor):
+class ClassInfoExtractor(ast.NodeVisitor):
     def __init__(self):
-        super().__init__()
         self.classes = []
 
-    def visit_ClassDef(self, node: cst.ClassDef) -> None:
-        class_name = node.name.value
+    def visit_ClassDef(self, node):
+        class_name = node.name
         attributes = []
+        for item in node.body:
+            if isinstance(item, ast.AnnAssign):
+                attr_name = item.target.id
+                attr_type = item.annotation.id
+                default = ast.unparse(item.value) if item.value else None
+                attributes.append(AttrInfo(attr_name, attr_type, default))
+        self.classes.append(ClassInfo(class_name, attributes))
+        self.generic_visit(node)
 
-        for element in node.body.body:
-            if isinstance(element, cst.SimpleStatementLine):
-                for item in element.body:
-                    if isinstance(item, cst.AnnAssign):
-                        attr_name = item.target.value
-                        attr_type = item.annotation.annotation.value
-                        # Default value handling is simplified; real scenarios may need more complex logic
-                        default = libcst.Module([]).code_for_node(item.value)
-                        attributes.append(AttrInfo(name=attr_name, type=attr_type, default=default))
-
-        self.classes.append(ClassInfo(name=class_name, attributes=attributes))
-
-def source_info(source: str) -> SourceInfo:
-    module = cst.parse_module(source)
+def source_info(source):
+    tree = ast.parse(source)
     extractor = ClassInfoExtractor()
-    module.visit(extractor)
-
-    return SourceInfo(classes=extractor.classes)
+    extractor.visit(tree)
+    return SourceInfo(extractor.classes)
