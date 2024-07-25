99a8224d683092ae2d7a56f20e45d09cf7e4cebe
Simone <simone.giacomelli@gmail.com>
2024-07-25 17:25:11 +0100

    html_add development - refactoring

--- tests/common/designer/html_edit_test.py
+++ tests/common/designer/html_edit_test.py
@@ -1,25 +1,18 @@
 from wwwpy.common.designer.html_edit import html_add, Position
 from wwwpy.common.designer.html_locator import Node
 
+# language=html
+html = "<div id='foo'><div></div><div id='target'></div></div>"
+path = [Node("DIV", 0, {'id': 'foo'}), Node("DIV", 1, {'id': 'target'})]
+
 
 def test_add_beforebegin():
-    # language=html
-    html = "<div id='foo'><div></div><div id='target'></div></div>"
-    path = [Node("DIV", 0, {'id': 'foo'}), Node("DIV", 1, {'id': 'target'})]
-
     actual = html_add(html, 'xyz', path, Position.beforebegin)
     # language=html
-    expect = "<div id='foo'><div></div>xyz<div id='target'></div></div>"
+    assert actual == "<div id='foo'><div></div>xyz<div id='target'></div></div>"
 
-    assert actual == expect
 
 def test_add_afterend():
-    # language=html
-    html = "<div id='foo'><div></div><div id='target'></div></div>"
-    path = [Node("DIV", 0, {'id': 'foo'}), Node("DIV", 1, {'id': 'target'})]
-
     actual = html_add(html, 'xyz', path, Position.afterend)
     # language=html
-    expect = "<div id='foo'><div></div><div id='target'></div>xyz</div>"
-
-    assert actual == expect
+    assert actual == "<div id='foo'><div></div><div id='target'></div>xyz</div>"
