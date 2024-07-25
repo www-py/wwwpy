2c347f9c3903a56418188b72b806b81fcf7d2d62
Simone <simone.giacomelli@gmail.com>
2024-07-25 15:35:38 +0100

    refactoring

--- src/wwwpy/common/designer/code_edit.py
+++ src/wwwpy/common/designer/code_edit.py
@@ -1,14 +1,31 @@
-import ast
-from dataclasses import dataclass
-from typing import List
-
 import libcst as cst
 
 from wwwpy.common.designer.code_info import Attribute, info
 from wwwpy.common.designer.code_strings import html_string_edit
 
 
-class AddFieldToClassTransformer(cst.CSTTransformer):
+def add_attribute(source_code: str, attr_info: Attribute):
+    module = cst.parse_module(source_code)
+    i = info(source_code)
+
+    transformer = _AddFieldToClassTransformer(i.classes[0].name, attr_info)
+    modified_tree = module.visit(transformer)
+
+    return modified_tree.code
+
+
+def add_component(source_code: str, attribute_name: str, attribute_type: str, html_piece: str) -> str:
+    named_html = html_piece.replace('#name#', attribute_name)
+
+    def manipulate_html(html):
+        return html + named_html
+
+    source1 = add_attribute(source_code, Attribute(attribute_name, attribute_type, 'wpc.element()'))
+    source2 = html_string_edit(source1, manipulate_html)
+
+    return source2
+
+class _AddFieldToClassTransformer(cst.CSTTransformer):
     def __init__(self, class_name, new_field: Attribute):
         super().__init__()
         self.class_name = class_name
@@ -54,25 +71,3 @@ class AddFieldToClassTransformer(cst.CSTTransformer):
 
             return updated_node.with_changes(body=updated_node.body.with_changes(body=new_body))
         return updated_node
-
-
-def add_attribute(source_code: str, attr_info: Attribute):
-    module = cst.parse_module(source_code)
-    i = info(source_code)
-
-    transformer = AddFieldToClassTransformer(i.classes[0].name, attr_info)
-    modified_tree = module.visit(transformer)
-
-    return modified_tree.code
-
-
-def add_component(source_code: str, attribute_name: str, attribute_type: str, html_piece: str) -> str:
-    named_html = html_piece.replace('#name#', attribute_name)
-
-    def manipulate_html(html):
-        return html + named_html
-
-    source1 = add_attribute(source_code, Attribute(attribute_name, attribute_type, 'wpc.element()'))
-    source2 = html_string_edit(source1, manipulate_html)
-
-    return source2
