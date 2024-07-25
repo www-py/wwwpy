e63aa192331b784815327f8638505421d8dca220
Simone <simone.giacomelli@gmail.com>
2024-07-25 12:46:09 +0100

    html node path/finder development

--- src/wwwpy/common/designer/html_parser.py
+++ src/wwwpy/common/designer/html_parser.py
@@ -5,7 +5,6 @@ from typing import Tuple, Dict, List
 
 @dataclass()
 class CstNode:
-    """Concrete syntax tree node"""
     tag_name: str
     position: Tuple[int, int]
     children: List['CstNode'] = field(default_factory=list)
@@ -17,45 +16,38 @@ class _PositionalHTMLParser(HTMLParser):
         super().__init__()
         self.data = data
         self._tree: List[CstNode] = []
-        self._stack: List[CstNode] = []
-        self.current_pos = 0
+        self.stack: List[CstNode] = []
 
     def handle_starttag(self, tag, attrs):
-        start_pos = self.getpos()
-        node = CstNode(tag_name=tag, position=(start_pos[1], -1), attributes=dict(attrs))
-        if self._stack:
-            self._stack[-1].children.append(node)
-        else:
-            self._tree.append(node)
-        self._stack.append(node)
+        position = (self.getpos()[1], self.getpos()[1] + len(tag) + 2)  # Rough estimation of the tag length
+        attributes = {k: v for k, v in attrs}
+        node = CstNode(tag_name=tag, position=position, attributes=attributes)
+        if self.stack:
+            self.stack[-1].children.append(node)
+        self.stack.append(node)
 
     def handle_endtag(self, tag):
-        end_pos = self.getpos()
-        if self._stack:
-            node = self._stack.pop()
-            node.position = (node.position[0], end_pos[1])
+        if self.stack:
+            node = self.stack.pop()
+            node.position = (node.position[0], self.getpos()[1] + len(tag) + 3)
+            if not self.stack:
+                self._tree.append(node)
 
     def handle_startendtag(self, tag, attrs):
-        pos = self.getpos()
-        node = CstNode(tag_name=tag, position=(pos[1], pos[1]), attributes=dict(attrs))
-        if self._stack:
-            self._stack[-1].children.append(node)
+        position = (self.getpos()[1], self.getpos()[1] + len(tag) + 3)  # Rough estimation of the tag length
+        attributes = {k: v for k, v in attrs}
+        node = CstNode(tag_name=tag, position=position, attributes=attributes)
+        if self.stack:
+            self.stack[-1].children.append(node)
         else:
             self._tree.append(node)
 
     def handle_data(self, data):
-        self.current_pos += len(data)
+        pass  # Data is ignored for now
 
     def parse(self):
         self.feed(self.data)
         return self._tree
-
-def main(html_data):
-    parser = _PositionalHTMLParser(html_data)
-    position_map = parser.parse()
-
-    for pos in position_map:
-        print(pos)
 
 
 def html_to_tree(html: str) -> List[CstNode]:
