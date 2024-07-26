3822298c9ce03ffadfdb07a6ec33cc93086c728e
Simone <simone.giacomelli@gmail.com>
2024-07-26 12:39:51 +0100

    target_location development

--- src/wwwpy/remote/designer/target_path.py
+++ src/wwwpy/remote/designer/target_path.py
@@ -1,13 +1,57 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+from time import sleep
 from typing import List
 
-from js import HTMLElement, Array
+from js import HTMLElement, Array, Element
 
 from wwwpy.common.designer.html_locator import Node, NodePath
+from wwwpy.remote.component import Component
 
 
-def target_path(target: HTMLElement) -> NodePath:
+@dataclass()
+class TargetLocation:
+    """This class represents the location of a target relative to a Component."""
+    component: Component
+    """The Component that contains the target."""
+    path: NodePath
+    """The path from the Component (excluded) to the target."""
+
+
+def target_location(target: Element) -> TargetLocation:
+    """
+    Compute the [TargetLocation].
+    """
+
+    path = []
+    element: Element = target
+    while element:
+        if hasattr(element, "_py"):
+            component = element._py
+            return TargetLocation(component=component, path=path)
+
+        parent = element.parentNode
+        child_index = Array.prototype.indexOf.call(parent.children, element) if parent else -1
+        from js import console
+        from pyodide.ffi import to_js
+        try:
+            console.log('=' * 40)
+            console.log(f'has attr _py:{hasattr(element, "_py")}')
+            console.log('element :)', element.outerHTML[0:100])
+            attributes = {attr.name: attr.value for attr in element.attributes}
+        except:
+            attributes = {}
+            import traceback
+            console.log(traceback.print_exc())
+            sleep(10)
+        path.insert(0, Node(element.tagName, child_index, attributes))
+        element = parent
+
+    return TargetLocation(component=None, path=path)
+
+
+def path_to_target(target: HTMLElement) -> NodePath:
     """
     Get the path from the root to the target.
     """
--- tests/remote/designer/target_path_test.py
+++ tests/remote/designer/target_path_test.py
@@ -1,4 +1,8 @@
-from wwwpy.remote.designer.target_path import target_path
+import sys
+from pathlib import Path
+from time import sleep
+from tests.common import restore_sys_path
+from wwwpy.remote.designer.target_path import path_to_target, target_location, TargetLocation
 from wwwpy.common.designer.html_locator import Node
 from js import document, console
 
@@ -7,10 +11,57 @@ def test_target_path():
     div = document.createElement("div")
     div.innerHTML = """<div id='foo'><div></div><div id="target"></div></div>"""
     target = div.querySelector("#target")
-    actual = target_path(target)
+    actual = path_to_target(target)
     expect = [Node("DIV", -1, {}), Node("DIV", 0, {'id': 'foo'}), Node("DIV", 1, {'id': 'target'})]
     console.log(f'actual={actual}')
     assert actual == expect, f'\nexpect={expect} \nactual={actual}'
+
+
+def test_target_path_to_component(tmp_path, restore_sys_path):
+    # GIVEN
+
+    (tmp_path / 'component1.py').write_text('''import js
+
+import wwwpy.remote.component as wpc
+
+
+class Component1(wpc.Component):
+    btn1: js.HTMLButtonElement = wpc.element()
+
+    def connectedCallback(self):
+        self.element.innerHTML = """
+        <div></div>
+        <div class='class1'>foo
+            <button data-name='btn1' id='btn1id'>bar</button>
+        </div>
+    """
+    ''')
+    sys.path.insert(0, str(tmp_path))
+    from component1 import Component1
+    # from tests.remote.designer.target_path_support.component1 import Component1
+    component1 = Component1()
+
+    # t = component1.component_metadata.tag_name
+    # document.body.innerHTML = f'<{t}></{t}>'
+    # document.body.innerHTML = f'<div>ciaooo</div>'
+    document.body.appendChild(component1.element)
+
+    # WHEN
+    target = document.querySelector("#btn1id")
+    assert target
+    actual = target_location(target)
+
+    # THEN
+    path = [Node("DIV", 1, {'class': 'class1'}),
+            Node("BUTTON", 0, {'data-name': 'btn1', 'id': 'btn1id'})]
+
+    expect = TargetLocation(component=component1, path=path)
+    if str(actual) != str(expect):
+        console.log(f'actual={actual}')
+        console.log(f'expect={expect}')
+        sleep(100)
+
+    assert str(actual) == str(expect)
 
 
 def disabled_test_html_locator__data_generator():
