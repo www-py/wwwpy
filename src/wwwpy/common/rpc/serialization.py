12cbad8c947ed4501d5eecefccb3bf25c38acc60
Simone <simone.giacomelli@gmail.com>
2024-07-28 21:49:02 +0100

    serialization

--- src/wwwpy/common/rpc/serialization.py
+++ src/wwwpy/common/rpc/serialization.py
@@ -1,19 +1,36 @@
 import json
 from dataclasses import is_dataclass, fields, dataclass
+from datetime import datetime
 from typing import Any, Type, get_origin, get_args
 
-def serialize(obj: Any) -> Any:
+
+# todo this should also receive the expected cls Type.
+# this way we can check that the instance is of the expected type
+def serialize(obj: Any, cls: Type) -> Any:
+    origin = get_origin(cls)
+    if origin is not None:
+        if not isinstance(obj, origin):
+            raise ValueError(f"Expected object of type {origin}, got {type(obj)}")
+    elif not isinstance(obj, cls):
+        raise ValueError(f"Expected object of type {cls}, got {type(obj)}")
     if is_dataclass(obj):
         return {
-            field.name: serialize(getattr(obj, field.name))
+            field.name: serialize(getattr(obj, field.name), field.type)
             for field in fields(obj)
         }
     elif isinstance(obj, list):
-        return [serialize(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {key: serialize(value) for key, value in obj.items()}
+        item_type = get_args(cls)[0]
+        return [serialize(item, item_type) for item in obj]
+    elif isinstance(obj, tuple):
+        return [serialize(item, get_args(cls)[i]) for i, item in enumerate(obj)]
+        # return [serialize(item) for item in obj]
+    # elif isinstance(obj, dict):
+    #     return {key: serialize(value) for key, value in obj.items()}
+    elif isinstance(obj, datetime):
+        return obj.isoformat()
     else:
         return obj
+
 
 def deserialize(data: Any, cls: Type) -> Any:
     if is_dataclass(cls):
@@ -22,22 +39,28 @@ def deserialize(data: Any, cls: Type) -> Any:
             field_name: deserialize(data.get(field_name), field_type)
             for field_name, field_type in field_types.items()
         })
-    elif get_origin(cls) == list:
+    elif get_origin(cls) == list or cls == list:
         item_type = get_args(cls)[0]
         return [deserialize(item, item_type) for item in data]
-    elif get_origin(cls) == dict:
-        key_type, value_type = get_args(cls)
-        return {
-            deserialize(key, key_type): deserialize(value, value_type)
-            for key, value in data.items()
-        }
+    elif get_origin(cls) == tuple or cls == tuple:
+        item_type = get_args(cls)[0]
+        return tuple(deserialize(item, item_type) for item in data)
+    # elif get_origin(cls) == dict:
+    #     key_type, value_type = get_args(cls)
+    #     return {
+    #         deserialize(key, key_type): deserialize(value, value_type)
+    #         for key, value in data.items()
+    #     }
+    elif cls == datetime:
+        return datetime.fromisoformat(data)
     else:
         return data
 
-def to_json(obj: Any) -> str:
-    return json.dumps(serialize(obj))
+
+def to_json(obj: Any, cls: Type) -> str:
+    return json.dumps(serialize(obj, cls))
+
 
 def from_json(json_str: str, cls: Type) -> Any:
     data = json.loads(json_str)
     return deserialize(data, cls)
-
--- tests/common/rpc/test_serialization.py
+++ tests/common/rpc/test_serialization.py
@@ -1,5 +1,9 @@
 # Example usage
 from dataclasses import dataclass
+from datetime import datetime
+from typing import List, Tuple
+
+import pytest
 
 from wwwpy.common.rpc import serialization
 
@@ -17,13 +21,17 @@ class Person:
     address: Address
 
 
-def test_ser():
-    expected = Person(name='John', age=30, address=Address(city='New York', zip_code=10001))
+john = Person(name='John', age=30, address=Address(city='New York', zip_code=10001))
 
-    serialized = serialization.to_json(expected)
+
+def test_ser():
+    expected = john
+
+    serialized = serialization.to_json(expected, Person)
     deserialized = serialization.from_json(serialized, Person)
 
     assert deserialized == expected
+
 
 def test_dc_datetime():
     from datetime import datetime
@@ -37,34 +45,60 @@ def test_dc_datetime():
 
     expected = Person(name='John', birthdate=datetime(2000, 1, 1))
 
-    serialized = serialization.to_json(expected)
+    serialized = serialization.to_json(expected, Person)
     deserialized = serialization.from_json(serialized, Person)
 
     assert deserialized == expected
 
 
-def test_dc_datetime():
+def test_datetime():
     from datetime import datetime
 
     expected = datetime(2000, 12, 31)
 
-    serialized = serialization.to_json(expected)
+    serialized = serialization.to_json(expected, datetime)
     deserialized = serialization.from_json(serialized, datetime)
 
     assert deserialized == expected
 
+
 def test_list():
     expected = [1, 2, 3]
 
-    serialized = serialization.to_json(expected)
-    deserialized = serialization.from_json(serialized, list)
+    serialized = serialization.to_json(expected, List[int])
+    deserialized = serialization.from_json(serialized, List[int])
 
     assert deserialized == expected
 
-def test_tuple():
-    expected = (1, 2, 3)
 
-    serialized = serialization.to_json(expected)
-    deserialized = serialization.from_json(serialized, tuple)
+def test_tuple():
+    expected = (1, 2.0, 'a')
+
+    serialized = serialization.to_json(expected, Tuple[int, float, str])
+    deserialized = serialization.from_json(serialized, Tuple[int, float, str])
+
+    assert deserialized == expected
+
+
+def test_wrong_type():
+    with pytest.raises(Exception):
+        serialization.to_json((datetime(2000, 12, 31),), datetime)  # it's a tuple
+    with pytest.raises(Exception):
+        serialization.to_json(john, datetime)
+
+def test_wrong_type2():
+    with pytest.raises(Exception):
+        serialization.to_json(Person('bob', 42, address=datetime(2000, 12, 31)), Person)
+
+def test_wrong_type3():
+    with pytest.raises(Exception):
+        serialization.to_json((1, 2.0, '3'), Tuple[int, float, int])
+
+
+def test_tuple_datetime():
+    expected = (datetime(2000, 12, 31),)
+
+    serialized = serialization.to_json(expected, Tuple[datetime])
+    deserialized = serialization.from_json(serialized, Tuple[datetime])
 
     assert deserialized == expected
