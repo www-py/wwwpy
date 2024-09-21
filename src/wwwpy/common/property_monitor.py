23314a62329cb5bdc20f3d1046b06a224b8fb903
Simone <simone.giacomelli@gmail.com>
2024-09-21 14:51:00 +0100

    property_monitor.py add test

--- src/wwwpy/common/property_monitor.py
+++ src/wwwpy/common/property_monitor.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass
 from typing import Callable, List
+from contextlib import contextmanager
 
 
 @dataclass
-class PropertyChange:
+class PropertyChanged:
     instance: any
     name: str
     old_value: object
     new_value: object
 
 
-def monitor_changes(instance, on_change: Callable[[List[PropertyChange]], None]):
+def monitor_changes(instance, on_changed: Callable[[List[PropertyChanged]], None]):
     """Monitor the changes of the properties of an instance of a class."""
-    if hasattr(instance, "__attr_change_monitor_on_change"):
+    if hasattr(instance, "__attr_change_monitor_on_changed"):
         raise Exception("The instance is already being monitored for property changes")
 
     clazz = instance.__class__
@@ -22,32 +23,31 @@ def monitor_changes(instance, on_change: Callable[[List[PropertyChange]], None])
         original_setattr = clazz.__setattr__  # Keep a reference to the original method
 
         def new_setattr(self, name, value):
-            if hasattr(self, name) and name != "__attr_change_monitor_on_change" and \
-                    hasattr(self, "__attr_change_monitor_on_change"):
-                change = PropertyChange(self, name, getattr(self, name), value)
-                self.__attr_change_monitor_on_change([change])
+            doit = (hasattr(self, name) and name != "__attr_change_monitor_on_changed" and
+                    hasattr(self, "__attr_change_monitor_on_changed"))
+            if doit:
+                change = PropertyChanged(self, name, getattr(self, name), value)
             original_setattr(self, name, value)
+            if doit:
+                self.__attr_change_monitor_on_changed([change])
 
         clazz.__setattr__ = new_setattr
 
-    instance.__attr_change_monitor_on_change = on_change
-
-
-from contextlib import contextmanager
+    instance.__attr_change_monitor_on_changed = on_changed
 
 
 @contextmanager
 def group_changes(instance):
-    old = instance.__attr_change_monitor_on_change
+    old = instance.__attr_change_monitor_on_changed
     buffer = []
 
     def _in_buffer(changes):
         buffer.extend(changes)
 
-    instance.__attr_change_monitor_on_change = _in_buffer
+    instance.__attr_change_monitor_on_changed = _in_buffer
     try:
         yield instance
     finally:
-        instance.__attr_change_monitor_on_change = old
+        instance.__attr_change_monitor_on_changed = old
         if buffer:
             old(buffer)
--- tests/common/property_monitor_test.py
+++ tests/common/property_monitor_test.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import List
 
 from wwwpy.common import property_monitor as pm
-from wwwpy.common.property_monitor import PropertyChange, monitor_changes
+from wwwpy.common.property_monitor import PropertyChanged, monitor_changes
 import pytest
 
 from wwwpy.common.rpc import serialization
@@ -15,9 +15,9 @@ class TestClass:
 
 
 def test_monitor_existing_property_change():
-    events: List[List[PropertyChange]] = []
+    events: List[List[PropertyChanged]] = []
 
-    def on_change(changes: List[PropertyChange]):
+    def on_change(changes: List[PropertyChanged]):
         events.append(changes)
 
     obj = TestClass("bob", 10)
@@ -25,17 +25,17 @@ def test_monitor_existing_property_change():
 
     obj.value = 20
 
-    assert events == [[PropertyChange(obj, "value", 10, 20)]]
+    assert events == [[PropertyChanged(obj, "value", 10, 20)]]
 
 
 def test_monitor_on_second_instance():
-    events1: List[List[PropertyChange]] = []
-    events2: List[List[PropertyChange]] = []
+    events1: List[List[PropertyChanged]] = []
+    events2: List[List[PropertyChanged]] = []
 
-    def on_change1(changes: List[PropertyChange]):
+    def on_change1(changes: List[PropertyChanged]):
         events1.append(changes)
 
-    def on_change2(change2: List[PropertyChange]):
+    def on_change2(change2: List[PropertyChanged]):
         events2.append(change2)
 
     obj1 = TestClass("alice", 10)
@@ -45,13 +45,24 @@ def test_monitor_on_second_instance():
 
     obj1.value = 1
 
-    assert events1 == [[PropertyChange(obj1, "value", 10, 1)]]
+    assert events1 == [[PropertyChanged(obj1, "value", 10, 1)]]
     assert events2 == []
 
     obj2.value = 2
 
-    assert events1 == [[PropertyChange(obj1, "value", 10, 1)]]
-    assert events2 == [[PropertyChange(obj2, "value", 20, 2)]]
+    assert events1 == [[PropertyChanged(obj1, "value", 10, 1)]]
+    assert events2 == [[PropertyChanged(obj2, "value", 20, 2)]]
+
+
+def test_event_should_fire_after_property_change():
+    obj = TestClass("alice", 10)
+
+    def on_change(_):
+        assert obj.value == 1
+
+    monitor_changes(obj, on_change)
+
+    obj.value = 1
 
 
 def test_double_monitor__should_raise_exception():
@@ -62,9 +73,9 @@ def test_double_monitor__should_raise_exception():
 
 
 def test_group_changes():
-    events: List[List[PropertyChange]] = []
+    events: List[List[PropertyChanged]] = []
 
-    def on_change(changes: List[PropertyChange]):
+    def on_change(changes: List[PropertyChanged]):
         events.append(changes)
 
     obj = TestClass("alice", 10)
@@ -76,16 +87,16 @@ def test_group_changes():
         obj.name = "bob"
         assert events == []
 
-    assert events == [[PropertyChange(obj, "value", 10, 1), PropertyChange(obj, "name", "alice", "bob")]]
+    assert events == [[PropertyChanged(obj, "value", 10, 1), PropertyChanged(obj, "name", "alice", "bob")]]
     events.clear()
 
     # verify immediate delivery of changes
     obj.value = 123
-    assert events == [[PropertyChange(obj, "value", 1, 123)]]
+    assert events == [[PropertyChanged(obj, "value", 1, 123)]]
 
 
 def test_deserialize():
-    def on_change(changes: List[PropertyChange]):
+    def on_change(changes: List[PropertyChanged]):
         raise Exception("Should not be called")
 
     obj = TestClass("alice", 10)
