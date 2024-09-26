cb54a1c07114250bd56fca828e585ced50c74075
Simone <simone.giacomelli@gmail.com>
2024-09-26 10:21:44 +0100

    update quickstart

--- src/wwwpy/common/quickstart/setup1/remote/component1.py
+++ src/wwwpy/common/quickstart/setup1/remote/component1.py
@@ -3,6 +3,7 @@ import js
 
 
 class Component1(wpc.Component, tag_name='component-1'):
+
     def init_component(self):
         # language=html
         self.element.innerHTML = """<span>component-1</span>"""
