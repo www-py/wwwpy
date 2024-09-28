f995e8bc2b2f5df01f9d672baa85849f781d80e8
Simone <simone.giacomelli@gmail.com>
2024-09-28 08:46:36 +0100

    comment

--- tests/remote/test_in_pyodide.py
+++ tests/remote/test_in_pyodide.py
@@ -1,10 +1,13 @@
-from js import document
 import pytest
+from js import document
+
 
 def test_first():
     document.body.innerHTML = '<input id="tag1" value="foo1">'
     assert document.getElementById('tag1').value == 'foo1'
 
+
 @pytest.mark.asyncio
 async def test_second():
-    pass
+    assert False, 'This sadly succeeds, because async tests are running correctly in Pyodide' + \
+        'See issue: https://github.com/pyodide/pyodide/issues/2221#issuecomment-2379624269'
