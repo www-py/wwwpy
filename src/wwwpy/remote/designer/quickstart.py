12d60c110be6ba3207ce2ded40a05a647e6c4b8f
Simone <simone.giacomelli@gmail.com>
2024-10-11 18:13:01 +0100

    quickstart dev

---
+++ src/wwwpy/remote/designer/quickstart.py
@@ -0,0 +1,22 @@
+from wwwpy.common.quickstart import quickstart_list
+from wwwpy.remote.designer.ui.draggable_component import new_window
+from wwwpy.remote.designer.ui.searchable_list_1 import SearchableList1, Item
+import js
+from pyodide.ffi import create_proxy
+
+
+def show_selector():
+    dg = new_window("Select a quickstart", closable=False)
+    dg.set_size('300px', '300px')
+    dg.set_position('40','40')
+    cmp1 = SearchableList1()
+    cmp1.items = [Item(qs.title, qs.description, {'quickstart': qs}) for qs in quickstart_list()]
+    cmp1.placeholder = 'Search or select below...'
+    dg.element.append(cmp1.element)
+    js.document.body.append(dg.element)
+
+    def item_click(event):
+        item = event.detail
+        dg.element.remove()
+
+    cmp1.element.addEventListener('item-click', create_proxy(item_click))
