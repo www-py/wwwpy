1139ddd29e6ca9d59770d747fd23c3574db99e3e
Simone <simone.giacomelli@gmail.com>
2024-10-13 11:45:17 +0100

    el_standard.py improvements

--- src/wwwpy/common/designer/el_standard.py
+++ src/wwwpy/common/designer/el_standard.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from wwwpy.common.designer.element_library import ElementDef, Help, EventDef
+from wwwpy.common.designer.element_library import ElementDef, Help, EventDef, AttributeDef
 
 
 def _standard_elements_def() -> List[ElementDef]:
@@ -8,12 +8,42 @@ def _standard_elements_def() -> List[ElementDef]:
         ElementDef(
             'button', 'js.HTMLButtonElement',
             help=Help('A clickable button.', 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button'),
+            attributes=[
+                AttributeDef('type', Help('The type of the button.', ''),
+                             values=['submit', 'reset', 'button'], default_value='button'),
+                AttributeDef('value', Help('The value of the button.', '')),
+            ],
             events=[EventDef('click', Help('The button was clicked.',
-                                           'https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event'))],
+                                           'https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event')),
+                    EventDef('dblclick', Help('The button was double-clicked.',
+                                              'https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event'))],
         ),
         ElementDef(
             'input', 'js.HTMLInputElement',
             help=Help('A field for entering text.', 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input'),
+            attributes=[
+                AttributeDef('type', Help('The type of control to display. The default type is text.', ''),
+                             values=['text', 'password', 'checkbox', 'radio', 'button', 'submit', 'reset', 'file',
+                                     'hidden', 'image', 'date', 'datetime-local', 'month', 'time', 'week',
+                                     'number', 'range', 'email', 'url', 'search', 'tel', 'color'],
+                             default_value='text'),
+                AttributeDef('value', Help('The value of the input field.', '')),
+                AttributeDef('placeholder', Help('A hint to the user of what can be entered in the field.', '')),
+                AttributeDef('disabled', Help('Whether the control is disabled.', ''), boolean=True),
+                AttributeDef('readonly', Help('Whether the control is read-only.', ''), boolean=True),
+                AttributeDef('required', Help('Whether the control is required for form submission.', ''),
+                             boolean=True),
+                AttributeDef('min', Help('The minimum value of the input field.', '')),
+                AttributeDef('max', Help('The maximum value of the input field.', '')),
+                AttributeDef('step', Help('The legal number intervals for the input field.', '')),
+                AttributeDef('pattern', Help('A regular expression that the input\'s value is checked against.', '')),
+                AttributeDef('autocomplete', Help('Whether the control is required for form submission.', ''),
+                             values=['on', 'off']),
+                AttributeDef('autofocus', Help('Whether the control should have input focus when the page loads.', ''),
+                             boolean=True),
+                AttributeDef('checked', Help('Whether the control is checked.', ''), boolean=True),
+
+            ],
             events=[
                 EventDef('input', Help('The input event fires when the value of the element has been changed '
                                        'as a direct result of a user action',
@@ -28,6 +58,8 @@ def _standard_elements_def() -> List[ElementDef]:
         ),
 
     ]
+
+
     for r in res:
         r.gen_html = _generateHtml
     return res
@@ -50,7 +82,6 @@ def _generateHtml(element_def: ElementDef, name: str) -> str:
     gen_html = func.get(tag_name, None)
     html = '\n' + gen_html() if gen_html else '' + ElementDef.default_gen_html(element_def, name)
     return html
-
 
 # [
 #     _comp('input', 'js.HTMLInputElement', '<input type="text" data-name="#name#" value="#name#">'),
