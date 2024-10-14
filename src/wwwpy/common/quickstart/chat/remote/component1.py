95098f5a4b976ee972a4a3ad389708f90e51994c
Simone <simone.giacomelli@gmail.com>
2024-10-14 14:13:46 +0100

    quickstart chat - super basic impl

--- src/wwwpy/common/quickstart/chat/remote/component1.py
+++ src/wwwpy/common/quickstart/chat/remote/component1.py
@@ -10,14 +10,17 @@ class Component1(wpc.Component, tag_name='component-1'):
     slInput1: js.HTMLElement = wpc.element()
     slIconButton1: js.HTMLElement = wpc.element()
     slTextarea1: js.HTMLElement = wpc.element()
+    form1: js.HTMLElement = wpc.element()
 
     def init_component(self):
         # language=html
         self.element.innerHTML = """<span>component-1</span>
-
-<sl-input data-name="slInput1" placeholder="Message" title=""></sl-input>
-<sl-button data-name="slButton1">Send</sl-button>
-<sl-textarea data-name="slTextarea1" placeholder="slTextarea1" rows="10">Messages...</sl-textarea>"""
+<form data-name="form1">
+    <sl-input data-name="slInput1" placeholder="Message" title="" required></sl-input>
+    <sl-button data-name="slButton1" type="submit">Send</sl-button>
+    <sl-textarea data-name="slTextarea1" placeholder="slTextarea1" rows="10">Messages...</sl-textarea>
+</form>
+"""
         self.element.insertBefore(RpcIssueAlert().element, self.element.firstChild)
 
         def new_message(message):
@@ -30,5 +33,13 @@ class Component1(wpc.Component, tag_name='component-1'):
     async def slButton1__click(self, event):
         js.console.log('handler slButton1__click event =', event)
         import server.rpc
-        await server.rpc.send_message_to_all(self.slInput1.value)
-        self.slInput1.value = ''
+        try:
+            await server.rpc.send_message_to_all(self.slInput1.value)
+            self.slInput1.value = ''
+            self.slInput1.setCustomValidity('')
+        except Exception as e:
+            self.slInput1.setCustomValidity('Issues sending message... ' + str(e))
+            self.slInput1.reportValidity()
+
+    def form1__submit(self, event):
+        event.preventDefault()
