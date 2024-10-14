4a051420641d8dc2491bff2b2c73b25c060311fc
Simone <simone.giacomelli@gmail.com>
2024-10-14 17:30:28 +0100

    asgi incubator - refactoring

--- src/wwwpy/asgi/echo_handler.py
+++ src/wwwpy/asgi/echo_handler.py
@@ -1,10 +1,12 @@
 import os
 
+
 async def app(scope, receive, send):
     if scope['type'] == 'http':
         await handle_http(scope, send)
     elif scope['type'] == 'websocket':
         await handle_websocket(scope, receive, send)
+
 
 async def handle_http(scope, send):
     if scope['path'] == '/':
@@ -12,22 +14,20 @@ async def handle_http(scope, send):
     else:
         await send_response(200, b'Hello, world!', 'text/plain', send)
 
-async def handle_websocket(scope, receive, send):
-    if scope['path'] == '/echo':
-        await send({
-            'type': 'websocket.accept'
-        })
 
-        while True:
-            message = await receive()
-            if message['type'] == 'websocket.receive':
-                text = message.get('text')
-                await send({
-                    'type': 'websocket.send',
-                    'text': f"echo -> {text}"
-                })
-            elif message['type'] == 'websocket.disconnect':
-                break
+async def handle_websocket(scope, receive, send):
+    if scope['path'] != '/echo':
+        return
+    await send({'type': 'websocket.accept'})
+
+    while True:
+        message = await receive()
+        if message['type'] == 'websocket.receive':
+            text = message.get('text')
+            await send({'type': 'websocket.send', 'text': f"echo -> {text}"})
+        elif message['type'] == 'websocket.disconnect':
+            break
+
 
 async def serve_file(filename, content_type, send):
     try:
@@ -37,15 +37,11 @@ async def serve_file(filename, content_type, send):
     except FileNotFoundError:
         await send_response(404, b'File not found', 'text/plain', send)
 
+
 async def send_response(status, body, content_type, send):
     await send({
         'type': 'http.response.start',
         'status': status,
-        'headers': [
-            [b'content-type', content_type.encode()],
-        ],
+        'headers': [[b'content-type', content_type.encode()], ],
     })
-    await send({
-        'type': 'http.response.body',
-        'body': body,
-    })
+    await send({'type': 'http.response.body', 'body': body, })
