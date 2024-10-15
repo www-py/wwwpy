4448496b6dc7b7be04b6b6db91737cab55be0a29
Simone <simone.giacomelli@gmail.com>
2024-10-15 13:44:12 +0100

    fix scrambled log order

--- src/wwwpy/remote/designer/log_redirect.py
+++ src/wwwpy/remote/designer/log_redirect.py
@@ -4,14 +4,22 @@ from wwwpy.common.designer import log_emit
 from wwwpy.remote import set_timeout
 from wwwpy.server.designer import rpc
 
+_log_buffer = []
+
+
+async def _process_buffer():
+    """This techique avoids the issue of wrong log order"""
+    loc = _log_buffer.copy()
+    _log_buffer.clear()
+
+    for msg in loc:
+        await rpc.server_console_log(msg)
+
 
 def redirect_logging():
     def emit(msg: str):
         console.log(msg)
-
-        async def _():
-            await rpc.server_console_log(msg)
-
-        set_timeout(_)
+        _log_buffer.append(msg)
+        set_timeout(_process_buffer)
 
     log_emit.add_once(emit)
